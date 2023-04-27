fb463bcf5ca8b0a245caca9092224af7b3968032
Jacob DeMong <jdemong24@hanoverstudents.org>
2023-04-27 12:09:59 -0400

    Contact form updates

--- web/contact.php
+++ web/contact.php
@@ -11,6 +11,24 @@
 ?>
 
 <div class="jumbotron col-md-8 col-md-offset-2">
-    <h2>Contact the Site Developer or NHS Pillars</h2>
-    <p>If you would like to contact us, please email <a href="mailto:pidaychallenge@gmail.com">jdemong24@hanoverstudents.org</a>.</p>
+    <h2>Contact the Site Developer or NHS Officers</h2>
+    <p>If you would like to contact us, please email:
+    <br>
+    <a href="mailto:smarshall-pelayo23@hanoverstudents.org">Sophie Marshall Pelayo (president)</a>
+    <br>
+    <a href="mailto:khandrahan23@hanoverstudents.org">Kylie Handrahan (Vice President)</a>
+    <br>
+    <a href="mailto:clopes23@hanoverstudents.org">Cassie Lopes (Community Service Coordinatior)</a>
+    <br>
+    <a href="mailto:tmccrann23@hanoverstudents.org">Teddy McCrann (Treasurer)</a>
+    <br>
+    <a href="mailto:mgrace23@hanoverstudents.org">Mary Grace (Community Service Coordinatior)</a>
+    <br>
+    <a href="mailto:kshields23@hanoverstudents.org">Kathrine Shields (Secretary)</a></p>
+    <br>
+    <br>
+    <p> For site related questions, email:
+    <br>
+    <a href="mailto:jdemong24@hanoverstudents.org">Jacob DeMong (Developer)</a></p>
+
 </div>
